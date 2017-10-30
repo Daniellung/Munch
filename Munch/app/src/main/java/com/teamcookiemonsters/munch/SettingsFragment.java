3a0c8b1d99895672f0d5a4b8cd15d7e26bca344b
unknown <midquan@ucsc.edu>
2017-10-30 01:26:14 -0700

    Laptop

---
+++ Munch/app/src/main/java/com/teamcookiemonsters/munch/SettingsFragment.java
@@ -0,0 +1,30 @@
+package com.teamcookiemonsters.munch;
+
+/*import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
+*/
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(res.xml.preferences);
+    }
+    PreferenceManager.setDefaultValues(this, res.xml.preferences, false);
+}
