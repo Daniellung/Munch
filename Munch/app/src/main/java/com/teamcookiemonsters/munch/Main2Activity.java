9c898c403b8e115a6893833717e943f95bd26b54
Sanjeet Sanhotra <ssanhotr@ucsc.edu>
2017-11-19 11:20:12 -0800

    adds yelp

--- Munch/app/src/main/java/com/teamcookiemonsters/munch/Main2Activity.java
+++ Munch/app/src/main/java/com/teamcookiemonsters/munch/Main2Activity.java
@@ -4,19 +4,43 @@ package com.teamcookiemonsters.munch;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.yelp.fusion.client.connection.YelpFusionApi;
+import com.yelp.fusion.client.connection.YelpFusionApiFactory;
+import com.yelp.fusion.client.models.Business;
+import com.yelp.fusion.client.models.SearchResponse;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.*;
+import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
+//import okhttp3.Call;
+//import okhttp3.Callback;
+//import okhttp3.OkHttpClient;
+//import okhttp3.Request;
+//import okhttp3.Response;
+
+import static junit.framework.Assert.assertNotNull;
 
 public class Main2Activity extends AppCompatActivity
-    implements SearchView.OnQueryTextListener, SearchView.OnCloseListener{
+        implements SearchView.OnQueryTextListener, SearchView.OnCloseListener {
 
     private SearchManager searchManager;
     private android.widget.SearchView searchView;
@@ -26,10 +50,79 @@ public class Main2Activity extends AppCompatActivity
     private ArrayList<ParentRow> showTheseParentList = new ArrayList<ParentRow>();
     private MenuItem searchItem;
 
+    public String clientID = "eJuUlcITJQl5ZFtbE-Vs6g";
+    public String clientSecret = "q79eV6MADDyFMboJWaBNg41qaxBsAiAkxhElntcYQr2FfaCJPigDfJn4hJvuq97A";
+    public String web = "https://api.yelp.com/oauth2/token";
+    public String accessToken = "h0hbwDp-INaIDxBnvI75S9GNpokFCIiUq6k8PG6ZSvS96o8pJFWLtBpvNGhXrb6qUVNoHdo_cifBSv-W5EooyI2AEceKbXlhHUWDsqz8A6bCCGk1wKkY8xIwnZ__WXYx";
+
+    Thread t;
+
+    public String testOne;
+    public YelpFusionApi mYelpFusionApi;
+    public YelpFusionApiFactory apiFactory;
+    public Map<String, String> mParams;
+    //SearchResponse response;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        apiFactory = new YelpFusionApiFactory();
+        try {
+            //yelpFusionApi = apiFactory.createAPI(clientID, clientSecret);
+            mYelpFusionApi = apiFactory.createAPI(accessToken);
+        } catch (IOException ex) {
+            Log.d("CREATION", "PLEASE WORK");
+            //Toast.makeText(this,"Didnt Work",Toast.LENGTH_LONG).show();
+        }
+
+        mParams = new HashMap<>();
+
+        //mParams.put("San Francisco", "indian food");
+
+        mParams.put("term", "indian food");
+        mParams.put("latitude", "40.581140");
+        mParams.put("longitude", "-111.914184");
+
+        new GetData().execute();
+
+        try {
+            // thread to sleep for 1000 milliseconds
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            Toast.makeText(this,"Didnt Work",Toast.LENGTH_LONG).show();
+        }
+
+        /*Call<SearchResponse> call = yelpFusionApi.getBusinessSearch(params);
+
+        Response<SearchResponse> response = null;
+
+
+
+        if (call != null){*/
+        /*Call<Business> call = yelpFusionApi.getBusiness("japacurry-truck-san-francisco");
+        Response<Business> response;
+
+        try {
+             response = call.execute();
+            Business business = response.body();
+        } catch (IOException e){
+            Toast.makeText(this,"Didnt Work",Toast.LENGTH_LONG).show();
+        }
+
+        */
+       /* try {
+          businessSearchTest();
+          //response = call.execute();
+        } catch (IOException ex){
+            Toast.makeText(this,"Didnt Work",Toast.LENGTH_LONG).show();
+        }*/
+
+        //response.body().getBusinesses().get(0);
+        //testOne = response.getBusinesses().get(0).getName();
+
+
 
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSuportActionBar(toolbar);
@@ -53,6 +146,20 @@ public class Main2Activity extends AppCompatActivity
         expandAll();
     }
 
+
+
+
+   /* public void businessSearchTest() throws IOException {
+        Map<String, String> parms = new HashMap<>();
+        parms.put("term", "indian food");
+        parms.put("latitude", "40.581140");
+        parms.put("longitude", "-111.914184");
+        Call<SearchResponse> call = mYelpFusionApi.getBusinessSearch(parms);
+        Response<SearchResponse> response = call.execute();
+        testOne = response.body().getBusinesses().get(0).getName();
+        assertNotNull(response);
+    }*/
+
     private void loadData() {
         ArrayList<ChildRow> childRows = new ArrayList<ChildRow>();
         ParentRow parentRow = null;
@@ -68,18 +175,19 @@ public class Main2Activity extends AppCompatActivity
         childRows.add(new ChildRow(R.mipmap.ic_launcher_round
                 , "Text3"));
         childRows.add(new ChildRow(R.mipmap.ic_launcher_round
-                , "Text4"));
+        , testOne));
         parentRow = new ParentRow("Second Group", childRows);
         parentList.add(parentRow);
     }
 
-    private void expandAll(){
+    private void expandAll() {
         int count = listAdapter.getGroupCount();
-        for (int i = 0; i < count; i++){
+        for (int i = 0; i < count; i++) {
             myList.expandGroup(i);
         }
     }
-    private void displayList(){
+
+    private void displayList() {
         loadData();
 
         myList = (ExpandableListView) findViewById(R.id.expandableListView_search);
@@ -88,7 +196,7 @@ public class Main2Activity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         //inflate the menu; this adds items to the aciton bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         searchItem = menu.findItem(R.id.action_search);
@@ -121,5 +229,25 @@ public class Main2Activity extends AppCompatActivity
         listAdapter.filterData(newText);
         expandAll();
         return false;
+    }
+
+
+    class GetData extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            Call<SearchResponse> call = mYelpFusionApi.getBusinessSearch(mParams);
+            Response<SearchResponse> response = null;
+            try {
+                response = call.execute();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            if(response != null){
+                Log.v("Businesses", response.body().getBusinesses().get(0).getName());
+                testOne = response.body().getBusinesses().get(0).getName();
+            }
+            return null;
+        }
     }
 }
