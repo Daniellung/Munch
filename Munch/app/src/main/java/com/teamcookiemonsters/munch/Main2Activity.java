b227e59c29748e8c9b457b33986e683d17c4894a
Justin Tse <jbtse@ucsc.edu>
2017-11-28 14:03:28 -0800

    added completed pick for me button that picks random restaurant from current list

--- Munch/app/src/main/java/com/teamcookiemonsters/munch/Main2Activity.java
+++ Munch/app/src/main/java/com/teamcookiemonsters/munch/Main2Activity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.SearchView;
@@ -74,6 +75,8 @@ public class Main2Activity extends AppCompatActivity
     String[] lists = new String[20];
 
     //public int i;
+    public int randomIndex;
+    public String restaurantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -181,6 +184,23 @@ public class Main2Activity extends AppCompatActivity
             }
         });
         */
+        // connect random button
+        Button randomButton = (Button) findViewById(R.id.random_button);
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get random index
+                 randomIndex = getRandomInt();
+                 // get restaurant name
+                 restaurantName = lists[randomIndex];
+
+                 // launch full view activity
+                Intent restInfoIntent = new Intent((Main2Activity.this), RestInfoActivity.class);
+                // pass restaurant name to the activity
+                restInfoIntent.putExtra("name", restaurantName);
+                startActivity(restInfoIntent);
+            }
+        });
     }
 
     /*public void setSearch(){
---
+++ Munch/app/src/main/java/com/teamcookiemonsters/munch/RestInfoActivity.java
@@ -0,0 +1,34 @@
+package com.teamcookiemonsters.munch;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class RestInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest_info);
+
+        String name = getIntent().getStringExtra("name");
+        TextView testText = (TextView) findViewById(R.id.testText);
+        testText.setText(name);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
