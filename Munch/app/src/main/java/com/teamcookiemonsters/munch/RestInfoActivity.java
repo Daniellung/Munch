7d5c9aee6317bdb9c701b41475af3c3dabad276c
Justin Tse <jbtse@ucsc.edu>
2017-11-30 21:50:50 -0800

    added a randomize button leads to a restaurant info display page

--- Munch/app/src/main/java/com/teamcookiemonsters/munch/RestInfoActivity.java
+++ Munch/app/src/main/java/com/teamcookiemonsters/munch/RestInfoActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class RestInfoActivity extends AppCompatActivity {
 
     @Override
@@ -18,13 +20,31 @@ public class RestInfoActivity extends AppCompatActivity {
         // receive passed in restaurant info
         String name = getIntent().getStringExtra("name");
         String phone = getIntent().getStringExtra("phone");
+        String address1 = getIntent().getStringExtra("address1");
+        String address2 = getIntent().getStringExtra("address2");
+        String address3 = getIntent().getStringExtra("address3");
+        String city = getIntent().getStringExtra("city");
+        String state = getIntent().getStringExtra("state");
+        String country = getIntent().getStringExtra("country");
         //String url = getIntent().getStringExtra("url");
 
         // display texts
         TextView phoneText = (TextView) findViewById(R.id.phone_text);
+        TextView address1Text = (TextView) findViewById(R.id.address1_text);
+        TextView address2Text = (TextView) findViewById(R.id.address2_text);
+        TextView address3Text = (TextView) findViewById(R.id.address3_text);
+        TextView cityText = (TextView) findViewById(R.id.city_text);
+        TextView stateText = (TextView) findViewById(R.id.state_text);
+        TextView countryText = (TextView) findViewById(R.id.country_text);
         //TextView urlText = (TextView) findViewById(R.id.url_text);
 
         phoneText.setText(phone);
+        address1Text.setText(address1);
+        address2Text.setText(address2);
+        address3Text.setText(address3);
+        cityText.setText(city);
+        stateText.setText(state);
+        countryText.setText(country);
         //urlText.setText(url);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
